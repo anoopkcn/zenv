b81c9b07e376dc98a78496394824230b5011fc8f
Anoop K. Chandran <anoopkcn@gmail.com>
2025-04-29 15:51:33 +0200

    Few help message changes

--- src/main.zig
+++ src/main.zig
@@ -44,25 +44,21 @@ fn printUsage() void {
         \\Configuration (zenv.json):
         \\  The zenv.json file defines your environments. It can optionally include top-level key-value:
         \\  "base_dir": "path/to/venvs",  Specifies the base directory for creating virtual environments.
-        \\  Can be relative to zenv.json location or an absolute path.
-        \\  Defaults to "zenv" if omitted.
+        \\  Can be relative to zenv.json location or an absolute path. Defaults to "zenv" if omitted.
         \\
         \\Commands:
         \\  init                      Create a new zenv.json template file in the current directory.
         \\
-        \\  setup <env_name>          Set up the specified environment for the current machine.
-        \\                            Creates a Python virtual environment in <base_dir>/<env_name>/.
-        \\                            Checks if current machine matches env_name's target_machine.
+        \\  setup <env_name>          Set up the specified environment based on zenv.json file.
+        \\                            Creates a virtual environment in <base_dir>/<env_name>/.
         \\
         \\  activate <env_name|id>    Output the path to the activation script.
         \\                            You can use the environment name or its ID (full or partial).
-        \\                            To activate the environment, use:
-        \\                            source $(zenv activate <env_name|id>)
+        \\                            To activate the environment, use: source $(zenv activate <env_name|id>)
         \\
         \\  cd <env_name|id>          Output the project directory path.
         \\                            You can use the environment name or its ID (full or partial).
-        \\                            To change to the project directory, use:
-        \\                            cd $(zenv cd <env_name|id>)
+        \\                            To change to the project directory, use: cd $(zenv cd <env_name|id>)
         \\
         \\  list                      List environments registered for the current machine.
         \\
@@ -72,13 +68,14 @@ fn printUsage() void {
         \\                            Registers the current directory as the project directory.
         \\
         \\  deregister <env_name|id>  Remove an environment from the global registry.
+        \\                            It does not remove the environment itself.
         \\
         \\  version, -v, --version    Print the zenv version.
         \\
         \\  help, --help              Show this help message.
         \\
         \\Options:
-        \\  --force-deps              When used with setup command, it tries to install all specified dependencies
+        \\  --force-deps              When used with setup command, it tries to install all dependencies
         \\                            even if they are already provided by loaded modules.
         \\
         \\  --no-host                 Bypass hostname validation and allow setup/register of an environment
