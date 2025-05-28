f3a6db137199c8461937052a3f98ddc2b9a4e4e5
Anoop K. Chandran <anoopkcn@gmail.com>
2025-05-28 23:01:48 +0200

    rename and cleanup template

--- src/utils/template_json.zig
+++ src/utils/template_json.zig
@@ -2,24 +2,11 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 const template = @import("template.zig");
 
-// Embed the template file at compile time
-// const JSON_TEMPLATE = @embedFile("templates/zenv.json.template");
-const JSON_CUSTOM_TEMPLATE = @embedFile("templates/zenv.json.custom.template");
+const JSON_CUSTOM_TEMPLATE = @embedFile("templates/zenv.json.template");
 
-/// Creates a templated JSON configuration file with provided replacements
-// pub fn createJsonConfigFromTemplate(
-//     allocator: Allocator,
-//     replacements: std.StringHashMap([]const u8),
-// ) ![]const u8 {
-//     // Process the template
-//     return try template.processTemplateString(allocator, JSON_TEMPLATE, replacements);
-// }
-
-/// Creates a custom templated JSON configuration file with provided replacements
 pub fn createCustomJsonConfigFromTemplate(
     allocator: Allocator,
     replacements: std.StringHashMap([]const u8),
 ) ![]const u8 {
-    // Process the custom template
     return try template.processTemplateString(allocator, JSON_CUSTOM_TEMPLATE, replacements);
 }
